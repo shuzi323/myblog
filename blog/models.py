fd0cea59677b705d6f7b27fb18466f42ce39dabe
shuzi323 <1747882365@qq.com>
2017-10-02 20:17:51 +0800

    markdown

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.six import python_2_unicode_compatible
+
 
 class Category(models.Model):
     # 分类
@@ -13,6 +16,7 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+@python_2_unicode_compatible
 class Post(models.Model):
     # 文章
     title = models.CharField(max_length=70)
@@ -34,3 +38,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
