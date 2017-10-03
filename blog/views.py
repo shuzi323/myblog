1b0ae3c439c9560ac559a04f2d4473eaf4479982
shuzi323 <1747882365@qq.com>
2017-10-03 21:21:32 +0800

    修正已知小问题

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Category
+from comments.forms import CommentForm
 import markdown
 
 def index(request):
-    post_list = Post.objects.all().order_by('-created_time')
+    post_list = Post.objects.all()
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 def detail(request, pk):
@@ -15,7 +16,13 @@ def detail(request, pk):
                                       'markdown.extensions.codehilite',
                                       'markdown.extensions.toc',
                                       ])
-    return render(request, 'blog/detail.html', context={'post':post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
 
 def archives(request, year, month):
     # 归档页面
@@ -27,5 +34,5 @@ def archives(request, year, month):
 def category(request, pk):
     # 分类页面
     cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    post_list = Post.objects.filter(category=cate)
     return render(request, 'blog/index.html', context={'post_list': post_list})
